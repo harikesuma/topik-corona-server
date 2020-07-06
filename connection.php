048a2720b104a511f9b42f1abffec267858cd737
Made Hari Kesuma Arta <40531845+harikesuma@users.noreply.github.com>
2020-07-07 00:07:38 +0800

    change connection

--- connection.php
+++ connection.php
@@ -1,5 +1,8 @@
 <?php
-$mysqli = new mysqli("localhost","root","","db_topik_covid_bali");
+// $mysqli = new mysqli("localhost","root","","db_topik_covid_bali");
+
+$mysqli = new mysqli("cekkeuanganku.xyz","cekkeuangankuxyz_wincent","tugaswincent12345","cekkeuangankuxyz_db_topik_covid_bali");
+
 
 // Check connection
 if ($mysqli -> connect_errno) {
